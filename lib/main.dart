d0f5040d60a62f9cda1560c1dcb0e25318594a44
Ammar Shafiq <Ammardev99@gmail.com>
2024-06-26 17:03:59 +0500

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Color(0xFFE9F2FF),
+    statusBarBrightness: Brightness.light,
+  ));
+
   runApp(const MyApp());
 }
 
@@ -14,25 +22,25 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFE9F2FF)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: AnimatedSplashScreen(
+          duration: 2000,
+          splashTransition: SplashTransition.fadeTransition,
+          centered: true,
+          splash: SvgPicture.asset(
+            'assets/svg/app_logo.svg',
+            semanticsLabel: 'Widgets',
+            height: 100,
+          ),
+          nextScreen: const MyHomePage(title: 'Flutter Demo Home Page')),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
